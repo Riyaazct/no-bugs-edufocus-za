45024f47b20ce75a601577403175efd705f7d11e
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-02-15 13:35:59 +0200

    update server.js file with env variables for deployment

--- server/server.js
+++ server/server.js
@@ -15,4 +15,14 @@ server.on("listening", () => {
 
 process.on("SIGTERM", () => server.close(() => disconnectDb()));
 
-connectDb().then(() => server.listen(config.port));
+// use environment variables set on Netlify
+const dbHost = process.env.DB_HOST;
+const dbPort = process.env.DB_PORT;
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+const dbName = process.env.DB_NAME;
+
+// use the database configuration in your server code
+const dbUrl = `postgres://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`;
+
+connectDb(dbUrl).then(() => server.listen(config.port));
