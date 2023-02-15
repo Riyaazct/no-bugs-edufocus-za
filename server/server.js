5e72f39498eb3ab08eae9b5adfe6a8535b1986f3
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-02-15 14:27:12 +0200

    restore changes previously made in server.js

--- server/server.js
+++ server/server.js
@@ -15,14 +15,4 @@ server.on("listening", () => {
 
 process.on("SIGTERM", () => server.close(() => disconnectDb()));
 
-// use environment variables set on Netlify
-const dbHost = process.env.DB_HOST;
-const dbPort = process.env.DB_PORT;
-const dbUser = process.env.DB_USER;
-const dbPass = process.env.DB_PASS;
-const dbName = process.env.DB_NAME;
-
-// use the database configuration in your server code
-const dbUrl = `postgres://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`;
-
-connectDb(dbUrl).then(() => server.listen(config.port));
+connectDb().then(() => server.listen(config.port));
