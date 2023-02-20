a66239dee17f6917bbdadbfc024b382222447fea
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-02-20 13:23:21 +0200

    first commit

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,12 +1,7 @@
 const About = () => (
 	<main role="main">
 		<div>
-			<h1>About</h1>
-			<p>
-				Starter kit for full-stack JavaScript projects. For more information,
-				see the wiki:
-			</p>
-			<a href="https://github.com/textbook/starter-kit/wiki">Wiki</a>
+			<h1>About us</h1>
 		</div>
 	</main>
 );
