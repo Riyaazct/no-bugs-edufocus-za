791bc3a78454ebb20f1e201ae01f74d7e0a8bb08
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-02-20 20:18:44 +0200

    add image and text

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,9 +1,25 @@
+// import backgroundImg from "/public/bg.jpg";
+import "./about.css";
+
 const About = () => (
 	<main role="main">
-		<div>
-			<h1>About us</h1>
-		</div>
+		<section>
+			<div className="container text-center mt-5">
+				<h1 className="">About us</h1>
+				<div>
+					<img src="/images/bg.jpg" className="img-fluid" alt="/" />
+				</div>
+				<div>
+					<p>
+						EduFocus Projects NPO provides training, coaching and mentoring
+						interventions to the education sector, in alignment with Goal 4 of
+						the Sustainable Development Goals (SDGs, 2015): QUALITY EDUCATION.
+					</p>
+				</div>
+			</div>
+		</section>
 	</main>
 );
 
 export default About;
+
