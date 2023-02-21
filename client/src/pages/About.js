95061cbc9a3ec61ae6ccaff6e376f7ddf041ed85
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-02-21 02:47:58 +0200

    update layout to page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -3,23 +3,34 @@ import "./about.css";
 
 const About = () => (
 	<main role="main">
-		<section>
-			<div className="container text-center mt-5">
-				<h1 className="">About us</h1>
-				<div>
-					<img src="/images/bg.jpg" className="img-fluid" alt="/" />
-				</div>
-				<div>
-					<p>
-						EduFocus Projects NPO provides training, coaching and mentoring
-						interventions to the education sector, in alignment with Goal 4 of
-						the Sustainable Development Goals (SDGs, 2015): QUALITY EDUCATION.
-					</p>
-				</div>
+		<div className="ourStoryH1">
+			<h1>OUR STORY</h1>
+		</div>
+		<hr color="#1e0f9e" size="5" />
+		<div className="">
+			<div className="ourStoryImg">
+				<img src="/images/our-story.jpg" alt=".." />
 			</div>
-		</section>
+			<div className="ourStoryP">
+				<p>
+					<span>EduFocus Projects</span> is a 100% black women-led Non-Profit
+					Organisation that was established in 2020 during the Covid-19 hard
+					lockdown and temporary closure of school. Starting out as a passion
+					project, which emanated from the findings of a study done by Dr
+					Dorothy E. Esau, that revealed the need for support, guidance and
+					mentorship of novice teachers. We used social media (Facebook and
+					YouTube) as a virtual intervention strategy to reach our teachers to
+					bring hope, motivation and inspiration, especially to our younger
+					teachers, who had been particularly hard hit by the realities of a new
+					career in an abnormal working environment. EduFocus Projects has
+					subsequently been formalised as a Non-Profit Organisation,
+					specialising in training, coaching, mentoring and motivational
+					intervention programmes, which will hopefully, in the near future, be
+					rolled out to schools across the country.
+				</p>
+			</div>
+		</div>
 	</main>
 );
 
 export default About;
-
