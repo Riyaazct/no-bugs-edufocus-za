845e88a75384c520563dd6b5c7ac02c0736e900a
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-02-22 21:54:05 +0200

    add content for vision page

--- client/src/pages/Mission-Vision.js
+++ client/src/pages/Mission-Vision.js
@@ -1,37 +1,21 @@
 import React from "react";
 import "./Mission-Vision.css";
-const pageData = [
-	{
-		title: "OUR VISION",
-		statement:
-			'To become the leading service provider of intervention initiatives for teachers, in order to fulfill Goal 4 of the SDGs: "Ensuring inclusive and equitable quality education and promote lifelong learning opportunities for all".',
-		image: "images/vision.jpg",
-	},
-	{
-		title: "OUR MISSION",
-		statement:
-			"We empower and motivate teachers in South African schools by facilitating coaching, training, mentoring and motivational initiatives, special events and other research-based activities to amplify the teacher’s voice through reflexive practices.",
-		image: "images/mission.jpg",
-	},
-	{
-		title: "OUR OBJECTIVES",
-		statement: [
-			"To provide support, coaching and mentoring to teachers in the eight dimensions of holistic wellbeing i.e.(physical, psychological, social, financial, emotional, spiritual, vocational and environmental) in order to serve public school learners from previously disadvantaged communities.",
-			"To empower teachers with coping mechanisms and strategies to manage and refer learners from previous disadvantaged communities affected by social ills, to the relevant support structures.",
-			"To partner with the relevant stakeholders in order to equip teachers to become change agents in the schooling communities and communities at large.",
-			"To network and partner with local community-based organizations (CBOs) in order to identify and understand the unique context and needs of the community where the schools are located.",
-			"To assist teachers to identify the enabling and inhibiting factors which are unique to learners from previous disadvantaged communities in order to effectively implement successful teaching and learning in the schooling community.",
-			"To organize programmes for teachers exposing them to learn and practice new skills.",
-			"To contribute to the research community and to make recommendations to policy makers and education authorities about the implementation of a holistic mentorship programme in order to benefit stakeholders in previous disadvantaged communities.",
-		],
-		image: "images/objective.jpg",
-	},
-];
+
 const Mission = () => {
 	return (
 		<div className="container">
-			<div>
-
+			<h1 className="VisionH1">OUR VISION</h1>
+			<hr color="1e0f9e" size="5" />
+			<div className="visionImage">
+				<img src="/images/vision.jpg" alt="" />
+			</div>
+			<div className="visionParagraph">
+				<p>
+					To become the leading service provider of intervention initiatives for
+					teachers, in order to fulfill Goal 4 of the SDGs: "Ensuring inclusive
+					and equitable quality education and promote lifelong learning
+					opportunities for all".
+				</p>
 			</div>
 		</div>
 	);
