7bef41acb3e9467dde529439162f817b93fadd1e
chandredewet <chandredewet@gmail.com>
2023-02-22 07:23:25 +0200

    social media section completed

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -22,12 +22,31 @@ const Contact = () => (
 				alt="green blob"
 			/>
 			<h2 className="contactintroheading">Get in touch with us</h2>
-			<p className="contactintroblurb">
-				Click here to see what we've been up to <br /> and join our social media
-				community!
-			</p>
-		</section>
-		<section className="contactsocialmedia">
+			<div className="contactsocialcontainer">
+				<p className="contactintroblurb">
+				Click here to see what we've been up to <br /> and join our social media community! </p>
+				<section className="contactsocialmedia">
+					<img
+							className="contactintroicon contactintrogriditem"
+							src="/contact/facebook.svg"
+							alt="facebook icon"
+					/>
+					<img
+							className="contactintroicon contactintrogriditem"
+							src="/contact/youtube.svg"
+							alt="youtube icon"
+					/>
+					<img
+							className="contactintroicon contactintrogriditem"
+							src="/contact/dimensions8.png"
+							alt="youtube icon"
+					/>
+					<p className="contactintroicondesc contactintrogriditem">Please click here 
+						to watch all our videos</p>
+					<p className="contactintroicondesc contactintrogriditem"></p>
+					<p className="contactintroicondesc contactintrogriditem">Click here for our Eight Dimensions of Wellness Programme Overview</p>
+				</section>
+			</div>
 		</section>
 	</section>
 );
