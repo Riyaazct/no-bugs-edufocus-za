0a0065873e744e1b40b922019eb69b8eea8b23f8
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-02-26 02:54:26 +0200

    add layout and some styling

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
+import TheProblem from "./pages/TheProblem";
 
 const App = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
 		<Route path="/about/this/site" element={<About />} />
+		<Route path="/the-problem" element={<TheProblem />} />
 	</Routes>
 );
 
---
+++ client/src/pages/TheProblem.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./TheProblem.css";
+
+const TheProblem = () => {
+	return (
+		<div className="mainContainer" role={"main"}>
+			<div className="theProblemContent">
+				<div>
+					<h1>THE PROBLEM</h1>
+					<img src="/images/theproblem.jpg" alt="" />
+				</div>
+				<div>
+					<p>
+						Teaching is one of the most rewarding, yet highly stressful and
+						emotionally demanding professions in South Africa. Based on valuable
+						insights gained from novice teachers about their perceptions of
+						support to enhance their capabilities, this project highlights a
+						mentoring model of support to enhance teacher wellbeing as a
+						significant and necessary component towards holistic teacher
+						wellbeing. Despite the limited scientific data on the prevalence of
+						stress-related conditions in teaching, evidence do exist to indicate
+						that South African teachers display high levels of psychological
+						distress, burn-out and low morale. This is particularly the case in
+						schools where teachers face immense challenges like
+						learner-on-teacher violence; ill-disciplined learners; over-crowded,
+						under-resourced classrooms and unrealistic workloads. Following the
+						implementation of our programmes, we prepare Monitoring and
+						Evaluation Reports which provide recommendations that the Education
+						Department can implement in all public schools in their
+						constituency.
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default TheProblem;
