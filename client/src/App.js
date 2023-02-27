9ede174d6215a479a2ab7eefeba49846dd0a482a
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-02-27 16:10:21 +0200

    created dummy page until content recieved for page

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Publications from "./pages/Publications";
 
 const App = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
 		<Route path="/about/this/site" element={<About />} />
+		<Route path="/about/publications" element={<Publications />} />
 	</Routes>
 );
 
