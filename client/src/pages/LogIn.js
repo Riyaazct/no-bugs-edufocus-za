064a1ad0789ae4120c7f2951b311d5a35a2b2b0a
AlexJora <jora.catalinaa@gmail.com>
2023-02-27 14:03:50 +0200

    login form-useState/useEffect/handleSubmit

--- client/src/pages/LogIn.js
+++ client/src/pages/LogIn.js
@@ -1,37 +1,78 @@
-import React from 'react'
+import React from "react";
+import { useRef, useState, useEffect } from "react";
 
 function LogIn() {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, [])
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(user, pwd);
+    setUser("");
+    setPwd("");
+    setSuccess(true);
+  }
   return (
     <>
-      <div className='wrap'>
-        <h2>Log in</h2>
-        <form >
-          <label htmlFor="username">
-            Username:</label>
-          <input
-            type="text"
-            id="username"
-            autoComplete="off"
-            required />
-
-          <label htmlFor="password">
-            Password:
-          </label>
-          <input
-            type="password"
-            id="password"
-            required />
-
-          <button>Log in</button>
-        </form>
-        <p>
-          Don't have an account?<br />
-          <span className="line">
-            
-            <a href="#">Sign Up</a>
-          </span>
-        </p>
-      </div>
+      {success ? (
+        <section>
+          <h1>You are logged in!</h1>
+          <br />
+          <p>
+            <a href="#">Go to Member page</a>
+          </p>
+        </section>
+      ) : (
+        <section className='wrap'>
+          <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+          <h1>Sign In</h1>
+          <h2>Log in</h2>
+          <form onSubmit={handleSubmit}>
+            {/* USERNAME */}
+            <label htmlFor="username">
+              Username:</label>
+            <input
+              type="text"
+              id="username"
+              ref={userRef}
+              autoComplete="off"
+              onChange={(e) => setUser(e.target.value)}
+              value={user}
+              required 
+              />
+            {/* PASSWORD */}
+            <label htmlFor="password">
+              Password:
+            </label>
+            <input
+              type="password"
+              id="password"
+              onChange={(e) => setPwd(e.target.value)}
+              value={pwd}
+              required />
+            <button>Log in</button>
+          </form>
+          <p>
+            Don't have an account?<br />
+            <span className="line">
+               <a href="#">Sign Up</a>
+            </span>
+          </p>
+        </section>
+      )}
     </>
   )
 }
