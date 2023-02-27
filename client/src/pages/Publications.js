2f00cb2ea98fcd2edef656c7d35287fb250f20ef
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-02-28 00:18:24 +0200

    update after merge

--- client/src/pages/Publications.js
+++ client/src/pages/Publications.js
@@ -2,15 +2,14 @@ import React from "react";
 import "./Publications.css";
 
 const Publications = () => {
-    return (
-        <div className="container">
-            <div className="textContent">
-            <h1>Publications</h1>
-            <p>Page to follow at a later stage</p>
-            </div>
-        </div>
-
-    );
+	return (
+		<div className="container">
+			<div className="textContent">
+				<h1>Publications</h1>
+				<p>Page to follow at a later stage</p>
+			</div>
+		</div>
+	);
 };
 
 export default Publications;
