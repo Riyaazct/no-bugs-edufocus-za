f4ef11e0d05a365b2bcab42ada5945493dde1ca8
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-02-27 16:31:54 +0200

    create dummy page for our partners

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import About from "./pages/About";
 import Home from "./pages/Home";
+import Partners from "./pages/Partners";
+import "./pages/Partners.css";
 
 const App = () => (
 	<Routes>
 		<Route path="/" element={<Home />} />
 		<Route path="/about/this/site" element={<About />} />
+		<Route path="/about/partners" element={<Partners />} />
 	</Routes>
 );
 
---
+++ client/src/pages/Partners.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Partners = () => {
+	return (
+		<div className="container">
+			<div className="textContent">
+				<h1>OUR PARTNERS</h1>
+				<p>Page to follow at a later stage</p>
+			</div>
+		</div>
+	);
+};
+
+export default Partners;
