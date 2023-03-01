cf7237a0b62b9ad476e8bb985616a4505084731d
AlexJora <jora.catalinaa@gmail.com>
2023-03-01 10:23:36 +0200

    connecting with database

--- server/api.js
+++ server/api.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
-// import db from "./db";
+import db from "./db";
 import logger from "./utils/logger";
 
-const router = Router(db);
+const router = Router();
 
 router.get("/", (_, res) => {
 	logger.debug("Welcoming everyone...");
@@ -13,8 +13,8 @@ router.post("/users", async (req, res) => {
 	
 	try {
 		const {users, email, pwd} = req.body
-		const result = await db.none(
-			"INSERT INTO signup (users, pwd, email) VALUES ($1, $2, $3)",
+		const result = await db.query(
+			"INSERT INTO signup (users, email, pwd) VALUES ($1, $2, $3)",
 			[users, email, pwd]
 
 		);
--- server/app.js
+++ server/app.js
@@ -29,4 +29,11 @@ app.use(clientRouter(apiRoot));
 
 app.use(logErrors());
 
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+
 export default app;
