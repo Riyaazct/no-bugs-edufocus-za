dee401afa150ea7e77bad4cb302bef07c069ee51
AlexJora <jora.catalinaa@gmail.com>
2023-03-05 14:03:32 +0200

    RequireAuth component without roles

--- client/src/App.js
+++ client/src/App.js
@@ -9,17 +9,23 @@ import LogIn from "./pages/LogIn";
 import Member from "./pages/Member";
 import Error from "./pages/Error";
 import Unauthorized from "./pages/Unauthorize";
+import RequireAuth from "./components/RequireAuth";
 
 const App = () => (
 	<Routes>
+		{/* public routes */}
 		<Route path="/" element={<Home />} />
 		<Route path="/about" element={<About />} />
 		<Route path="/photos" element={<OurPhotos />} />
 		<Route path="/problem" element={<TheProblem />} />
 		<Route path="/login" element={<LogIn />} />
 		<Route path="/signup" element={<SignUp />} />
-		<Route path="/member" element={<Member />} />
 		<Route path="/unauthorized" element={<Unauthorized />} />
+		{/* protected routes */}
+		<Route element={<RequireAuth />}>
+		<Route path="/member" element={<Member />} />
+		</Route>
+		{/* error route */}
 		<Route path="*" element={<Error />} />
 	</Routes>
 );
