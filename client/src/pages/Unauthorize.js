a0189342cacb286dfbf05d776db2b847810d5726
AlexJora <jora.catalinaa@gmail.com>
2023-03-05 12:59:33 +0200

    adding useAuth hook and links in error and unautorized page

--- client/src/pages/Unauthorize.js
+++ client/src/pages/Unauthorize.js
@@ -1,11 +1,11 @@
 import React from "react";
-
+import { Link } from "react-router-dom"
 const Unauthorized = () => {
   return (
     <div className='unauthorized-page' style={{ textAlign: "center", marginTop: "3rem" }}>
       <div>HTTP Error 401: Unauthorized Access</div>
       <p>You have attempted to access a page for witch you are not authorized.</p>
-      <p>Back to Home Page</p>
+      <Link to="/">Visit Our Home Page</Link>
     </div>
   )
 }
