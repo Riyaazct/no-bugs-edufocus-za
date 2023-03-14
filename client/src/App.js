a3c79bc7e43174b4bb4d1151cece6ee81b14fec0
Riyaaz Singh <94745207+Riyaazct@users.noreply.github.com>
2023-03-14 20:38:18 +0200

    added page to about us section anad pulled latest changes from main branch

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,19 @@ import OurPeople from "./pages/OurPeople";
 import OurPhotos from "./pages/OurPhotos";
 // import TheProblem from "./pages/TheProblem";
 import LogIn from "./pages/LogIn";
-
 import Member from "./pages/Member";
 import Error from "./pages/Error";
 import Unauthorized from "./pages/Unauthorize";
 import RequireAuth from "./components/RequireAuth";
 import Administrator from "./pages/Administrator";
-
 import SignUp from "./pages/SignUp";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
-import Home from "./pages/Home";
 import Partners from "./pages/Partners";
 import "./pages/Partners.css";
-import OurPhotos from "./pages/OurPhotos";
-import TheProblem from "./pages/TheProblem";
-import SignUp from "./pages/SignUp";
-import LogIn from "./pages/LogIn";
+
+
+
 
 
 const App = () => (
