4e0ea82f5e8762bf9c2d655421fe382cd7caa0a2
chandredewet <chandredewet@gmail.com>
2023-03-16 10:21:22 +0200

    Our Programmes Page responsiveness styling complete

--- client/src/pages/OurProgrammes.js
+++ client/src/pages/OurProgrammes.js
@@ -10,18 +10,20 @@ const OurProgrammes= () => {
 		<section className="ourprogrammes">
             <h2 className="ourprogrammesheading">Our Programmes.</h2>
             <p className="ourprogrammesparagraphs"> Our programmes span across two geographical areas, namely:</p>
-               <div >
-                 <span className="ourprogrammetypes">A. Urban Educator Development</span>
-                 <span className="ourprogrammetypes">B. Rural Educator Development</span>
+               <div className="ourprogrammescontainer" >
+                 <p className="ourprogrammestypes">A. Urban Educator Development</p>
+                 <p className="ourprogrammestypes">B. Rural Educator Development</p>
                </div>
             <p className="ourprogrammesparagraphs"> Our focus areas are:</p>
-            <ul className="ourprogrammeslist">
-                <li>Life Skills Training</li>
-                <li>Communications Skills Training</li>
-                <li>Classroom Management Skills Training</li>
-                <li>Training in Enhancing Holistic Teacher Wellbeing</li>
-                <li>Mentor & Mentee Training</li>
-            </ul>
+            <div className="ourprogrammescontainer">
+                <ul className="ourprogrammeslist">
+                    <li>Life Skills Training</li>
+                    <li>Communications Skills Training</li>
+                    <li>Classroom Management Skills Training</li>
+                    <li>Training in Enhancing Holistic Teacher Wellbeing</li>
+                    <li>Mentor & Mentee Training</li>
+                </ul>
+            </div>
             <p className="ourprogrammesparagraphs">
                 In 2022, we successfully conducted an evidence-based, eight-week mentoring programme for novice teachers to enhance their holistic wellbeing in certain WCED schools. The programme is aimed at supporting their smooth transition from the university into the teaching profession and beyond. This will not only equip them with the much-needed and essential soft skills to master their classroom and pedagogical interactions, but it will also empower them to take ownership of their own wellbeing by providing them with the necessary tools to lead balanced personal lives.  </p>
             <p className="ourprogrammesparagraphs">
