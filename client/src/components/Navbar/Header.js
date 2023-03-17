588387d1b107f1eb64b428620f426bc71caa6c68
mr_maroga <45645052+AdvocateM@users.noreply.github.com>
2023-03-17 20:07:42 +0200

    Updated buttons

--- client/src/components/Navbar/Header.js
+++ client/src/components/Navbar/Header.js
@@ -3,78 +3,74 @@ import { useRef } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import "../../pages/OurPhotos.css";
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 
 function NavbarBlue() {
-	const navRef = useRef();
+  const navRef = useRef();
 
-	const showNavbar = () => {
-		navRef.current.classList.toggle("responsive_nav");
-	};
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
 
-	return (
-		<header>
-			<h3>EduFocus</h3>
-			<nav ref={navRef}>
-				<div className={location.pathname === "/" ? "pageActive" : ""}>
-					<a href="/">
-						Home
-					</a>
-				</div>
-				<div className={location.pathname === "/about" ? "pageActive" : ""}>
-					<a href="/about">
-						About Us
-					</a>
-				</div>
-				<div className={location.pathname === "/contact" ? "pageActive" : ""}>
-					<a href="/contact">
-						Contact Us
-					</a>
-				</div>
-				<div
-					className={
-						location.pathname === "/Ourpeople/this/a/team" ? "pageActive" : ""
-					}
-				>
-					<a href="/Ourpeople/this/a/team">
-						Our People
-					</a>
-				</div>
-				<div className={location.pathname === "/photos" ? "pageActive" : ""}>
-					<a href="/photos">Our Photos</a>
-				</div>
-				<div className="small-s">
-					<a href="/login">Login</a>
-					<a href="/signup">Sign Up</a>
-				</div>
+  return (
+    <header>
+      <h3>EduFocus</h3>
+      <nav ref={navRef}>
+        <div className={location.pathname === "/" ? "pageActive" : ""}>
+          <a href="/">
+            Home
+          </a>
+        </div>
+        <div className={location.pathname === "/about" ? "pageActive" : ""}>
+          <a href="/about">
+            About Us
+          </a>
+        </div>
+        <div className={location.pathname === "/contact" ? "pageActive" : ""}>
+          <a href="/contact">
+            Contact Us
+          </a>
+        </div>
+        <div
+          className={
+            location.pathname === "/Ourpeople/this/a/team" ? "pageActive" : ""
+          }
+        >
+          <a href="/Ourpeople/this/a/team">
+            Our People
+          </a>
+        </div>
+        <div className={location.pathname === "/photos" ? "pageActive" : ""}>
+          <a href="/photos">Our Photos</a>
+        </div>
+        <div className="small-s">
+          <a href="/login">Login</a>
+          <a href="/signup">Sign Up</a>
+        </div>
 
-				<button className="nav-btn nav-close-btn" onClick={showNavbar}>
-					<FaTimes />
-				</button>
-			</nav>
-			<button className="nav-btn" onClick={showNavbar}>
-				<FaBars />
-			</button>
+        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes />
+        </button>
+      </nav>
+      <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
 
-			<div className="header-buttons">
-				<div className="small-white-outline">
-					<div className="sign-up valign-text-middle dmsans-medium-white-16px">
-						<span>
-							<span className="dmsans-medium-white-16px">Sign in</span>
-						</span>
-					</div>
-				</div>
-				<Link to="/signup" className="no-link">
-					<div className="small-fill">
-						<div className="sign-up valign-text-middle dmsans-medium-white-16px">
-							<span>
-								<span className="dmsans-medium-white-16px">Sign Up</span>
-							</span>
-						</div>
-					</div>
-				</Link>
-			</div>
-		</header>
-	);
+      <div className="header-buttons">
+        <Stack spacing={2} direction="row">
+          <Link to="/login" className="no-link">
+            <Button variant="outlined">Login </Button>
+          </Link>
+          <Link to="/signup" className="no-link">
+            <Button variant="contained">Sign Up</Button>
+          </Link>
+        </Stack>
+      </div>
+    </header>
+
+  );
 }
+
 
 export default NavbarBlue;
